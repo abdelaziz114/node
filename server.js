760232b98c1b8742b0d79bf8ac0a5bbba9fe241f
abdelaziz114 <121435293+abdelaziz114@users.noreply.github.com>
2023-12-04 11:46:13 +0000

    changed to express

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-var http = require('http');
-console.log("hello world");
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+const express = require('express');
+var parseUrl = require('body-parser')
+
+const app = express();
+const port = 8080
+
+let encodeUrl = parseUrl.urlencoded({ extended: false })
+
+app.use(express.static('/'))
+app.get('/', (req, res) => {
+    //res.status(200).json({name:'name azerty'})
+    //res.send('Hello World!')
+    res.sendFile(__dirname + '/index.html')
+})
+app.post('/', encodeUrl, (req, res) => {
+    console.log('Form request:', req.body.input)
+    res.sendStatus(200)
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
